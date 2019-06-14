b2848ee1f4fb05ac95f7ee0491d2a338d8bcf1b5
Arthur Ribeiro <devarthurribeiro@gmail.com>
2019-06-14 05:17:34 -0300

    add vuex

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from './router';
 
 import Ionic from '@ionic/vue';
 import '@ionic/core/css/ionic.bundle.css';
+import store from './store'
 
 Vue.use(Ionic);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app');
