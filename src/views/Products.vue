369ff27547be0240034f6b76ed3667d960187c37
Arthur Ribeiro <devarthurribeiro@gmail.com>
2019-06-20 01:45:08 -0300

    Add página de produtos

---
+++ src/views/Products.vue
@@ -0,0 +1,14 @@
+<template>
+  <ion-content class="ion-padding">
+    <h1>Produtos</h1>
+    <ProductList/>
+  </ion-content>
+</template>
+
+<script>
+export default {
+  name: 'Products'
+}
+</script>
+
+<style></style>
